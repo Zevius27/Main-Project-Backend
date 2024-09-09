701c34cbc7fe7a5a5c5af96e2d79d408e170220d
zevius <zeviusalpha@gmail.com>
2024-09-09 12:30:03 +0530

    Added cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,47 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { log } from 'console';
+import fs from 'fs';
+  
+
+
+// Configuration
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+ 
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+      if(!localFilePath) return null
+      const response = await cloudinary.uploader.upload(localFilePath,{
+         resource_type: "auto",
+      })
+      console.log("File uploaded to Cloudinary  ", response.url);
+      return response;
+    } catch (error) {
+      fs.unlinkSync(localFilePath);//remove local file (temporarly saved one)
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+cloudinary.v2.uploader.upload("img",{
+
+}, (error, result) => {
+   if (error) {
+      console.log(error);
+   } else {
+      console.log(result);
+   }
+})
