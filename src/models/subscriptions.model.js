f3c1b0a574d3f09ca1b1fe59dd3e9ef7270722d5
zevius <zeviusalpha@gmail.com>
2024-09-17 16:52:51 +0530

    subscription model added

--- src/models/subscriptions.model.js
+++ src/models/subscriptions.model.js
@@ -1,4 +1,25 @@
 
+import mongoose,  {Schema} from "mongoose"; 
 
 
+
+const subscriptionSchema = new Schema({
+   subscriber: {
+      type : Schema.Types.ObjectId, // the user subscribed
+      ref: "User"
+   },
+   channel:{
+      type : Schema.Types.ObjectId,// the user whom we are subscribed
+      ref: "User"
+   }
+
+},
+{
+   timestamps:true
+}
+
+);
+
+
+export const subscription = mongoose.model("Subscription", subscriptionSchema);
 
