6267faef7fe6af2c109246c9b47a0f73d75016b0
zevius <zeviusalpha@gmail.com>
2024-09-19 11:27:19 +0530

    added controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -425,6 +425,54 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+   const user = await userModel.aggregate([
+      {
+         $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup:{ 
+            from:"Video",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+               {
+                  $lookup:{
+                     from:"User",
+                     localField:"owner",
+                     foreignField:"_id",
+                     as:"owner",
+                     pipeline:[
+                        {
+                           $project:{
+                              fullname:1,
+                              username:1,
+                              avatar:1
+                           }
+                        }
+                     ]
+                  }
+               }
+            ]        
+         }
+      },
+      {
+         $addFields:{
+            owner:{
+               $first:"$owner"
+            }
+         }
+      }
+   ])
+
+   return res
+      .status(200)
+      .json(new apiResponse(200, user[0].watchHistory, "watchHistory found successfully"))
+})
+
 
 export {
    registerUser,
@@ -436,5 +484,6 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverIMG,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
 }    
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOut, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logOut, registerUser, refreshAccessToken, currentPasswordChange, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverIMG, getUserChannelProfile } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.model.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -14,11 +14,11 @@ router.route("/register").post(
          message: "Avatar not uploaded, try again later "
       },
       {
-         name : "coverIMG",
+         name: "coverIMG",
          maxCount: 2,
          message: "CoverIMG not uploaded, try again later "
       }
-   ]),  
+   ]),
    registerUser)
 //http://localhost:8080/api/v1/users/register
 
@@ -33,6 +33,26 @@ router.route("/logout").post(
    logOut)
 
 router.route("/refreshToken").post(refreshAccessToken)
+
+
+router.route("/changePassword").post(verifyJWT, currentPasswordChange)
+
+router.route("/currrentUser").get(verifyJWT,
+   getCurrentUser
+)
+
+router.route("/updateAccount").patch(verifyJWT, updateAccountDetails)
+
+router.route("/updateAvatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar ) 
+
+router.route("/updateCoverIMG").patch(verifyJWT, upload.single("coverIMG"),updateUserCoverIMG )
+
+
+
+router.route("/profile/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watchHistory/:id").get(verifyJWT)
+
 
 
 
